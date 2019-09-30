1216c52b9ef22bfceb1a6a8837c7099e4abfb1be
Rohan Awhad <rohanavad007@gmail.com>
2019-10-01 03:31:51 +0530

    Pangolin works, with its own thread

--- slam.py
+++ slam.py
@@ -4,6 +4,9 @@ import time
 import numpy as np
 from frame import Frame, denormalize, match_frames, IRt
 import g2o
+from multiprocessing import Process, Queue
+import OpenGL.GL as gl
+import pangolin
 
 
 # camera intrinsics
@@ -19,15 +22,64 @@ class Map(object):
 	def __init__(self):
 		self.frames = []
 		self.points = []
+		#self.viewer_init()
+		self.state=None
+
+		self.q = Queue()
+		viewer = Process(target=self.viewer_thread, args=(self.q, ))
+		viewer.daemon = True
+		viewer.start()
+
+
+	def viewer_thread(self, q):
+		self.viewer_init()
+		while 1: 
+			self.viewer_refresh(q)
+
+
+	def viewer_init(self,):
+
+		pangolin.CreateWindowAndBind('Main', 640, 480)
+		gl.glEnable(gl.GL_DEPTH_TEST)
+
+		# Define Projection and initial ModelView matrix
+		self.scam = pangolin.OpenGlRenderState(
+			pangolin.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+			pangolin.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin.AxisDirection.AxisY))
+		self.handler = pangolin.Handler3D(self.scam)
+
+		# Create Interactive Window 
+		self.dcam = pangolin.CreateDisplay()
+		self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
+		self.dcam.SetHandler(self.handler)
+
+		self.state = None
+
+	def viewer_refresh(self, q):
+		if self.state is None or not q.empty():
+			self.state = q.get()
+		gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+		gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+		self.dcam.Activate(self.scam)
+
+		gl.glPointSize(10)
+		gl.glColor3f(0.0, 1.0, 0.0)
+		pangolin.DrawPoints(np.array([d[:3, 3] for d in self.state[0]]))
+		
+		gl.glPointSize(2)
+		gl.glColor3f(1.0, 0.0, 0.0)
+		pangolin.DrawPoints(np.array(self.state[1]))
+
+		pangolin.FinishFrame()
 
 
 	def display(self):
+		poses, pts = [], []
 		for f in self.frames:
-			print(f.id)
-			print(f.pose)
-			print()
-		#for p in points:
-		#	print(p.location)
+			poses.append(f.pose)
+		for p in self.points:
+			pts.append(p.pt)
+		self.q.put((poses, pts))
 
 # main classes
 disp = Display(W, H)
@@ -38,11 +90,12 @@ class Point(object):
 	# Each point is observed in multiple frames
 
 	def __init__(self, mapp, loc):
-		self.location = loc
+		self.pt = loc
 		self.frames = []
 		self.idxs = []
 		self.id = len(mapp.points)
 		mapp.points.append(self)
+
 
 	def add_observation(self, frame, idx):
 		self.frames.append(frame)
