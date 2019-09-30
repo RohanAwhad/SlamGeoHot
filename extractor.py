c584b68798806883882a2498c048b2a293178ad4
Rohan Awhad <rohanavad007@gmail.com>
2019-09-30 15:47:24 +0530

    Normalization and denormalization done

--- extractor.py
+++ extractor.py
@@ -3,11 +3,28 @@ import numpy as np
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 
-class Extractor:
-	def __init__(self):
+def add_ones(x):
+	#print(x.shape)
+	ret = np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+	#print(ret)
+	return ret
+
+
+class Extractor(object):
+	def __init__(self, K):
 		self.orb = cv2.ORB_create()
 		self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 		self.last = None
+		self.K = K
+		self.Kinv = np.linalg.inv(self.K)
+
+	def normalize(self, pts):
+		return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2]
+
+	def denormalize(self, pt):
+		ret = np.dot(self.K, np.array([pt[0], pt[1], 1.0]))
+		print(ret)
+		return int(round(ret[0])), int(round(ret[1]))
 
 	def extract(self, img):
 		
@@ -31,13 +48,24 @@ class Extractor:
 		# filter
 		if len(ret) > 0:
 			ret = np.array(ret)
-			model, iniliers = ransac((ret[:, 0], ret[:, 1]),
+			#print(ret.shape)
+			#print(ret)
+			#normalize coords: subtract to move to 0
+			ret[:,0,:] = self.normalize(ret[:, 0, :])
+			ret[:,1,:] = self.normalize(ret[:, 1, :])
+
+			#print(ret.shape)
+			#print(ret)
+
+			#print(ret)
+
+			model, inliers = ransac((ret[:, 0], ret[:, 1]),
 									FundamentalMatrixTransform,
 									min_samples=8,
 									residual_threshold=1,
 									max_trials=100)
-			ret = ret[iniliers]
-
+			ret = ret[inliers]
+			#print(ret.shape)
 		#return
 		self.last = {'kps' : kps, 'des' : des}
 		return ret
--- slam.py
+++ slam.py
@@ -8,15 +8,23 @@ W = 3840 // 4
 H = 2160 // 4
 
 disp = Display(W, H)
-fe = Extractor()
+F = 1
+K = np.array([[F, 0, W//2],[0,F,H//2], [0,0,1]])
+fe = Extractor(K)
 
 def process_frame(img):
 	img = cv2.resize(img, (W,H))
 	matches = fe.extract(img)
 
 	for pt1, pt2 in matches:
-		u1, v1 = map(lambda x: int(round(x)), pt1)
-		u2, v2 = map(lambda x: int(round(x)), pt2)
+		#u1, v1 = map(lambda x: int(round(x)), pt1)
+		#u2, v2 = map(lambda x: int(round(x)), pt2)
+
+		u1, v1 = fe.denormalize(pt1)
+		u2, v2 = fe.denormalize(pt2)
+
+		
+
 		cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
 		cv2.line(img, (u1, v1), (u2, v2), color=(255, 0, 0))
 
